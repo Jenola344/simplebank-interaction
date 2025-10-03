a986ea0510bf477cbcb6688a5c9071f6184723b4
Jenola <jesutolaolusegun@gmail.com>
2025-10-03 18:00:21 +0100

    commit

--- simplebank_flattened.sol
+++
@@ -1,7 +0,0 @@
-hat the path is correct and try again.
-At line:1 char:1
-+ contract balance: 0.00002 ETH
-+ ~~~~~~~~
-    + CategoryInfo          : ObjectNotFound: (contract:String) [], CommandNotFoundException     
-    + FullyQualifiedErrorId : CommandNotFoundException
- 
